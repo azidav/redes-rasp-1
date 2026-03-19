157464149eacdd8239d10fa8d7b23c401853668c
Kevin Silva <kevin-silva1998@hotmail.com>
2026-03-19 19:07:08 +0000

    modified:   app.py

--- app.py
+++ app.py
@@ -1,8 +1,4 @@
 from flask import Flask, request, jsonify, render_template
-from flask_cors import CORS
-
-app = Flask(__name__)
-CORS(app)
 import json
 from datetime import datetime
 import os
@@ -15,7 +11,6 @@ logging.basicConfig(
 )
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}}, allow_headers="*")
 
 ARQUIVO_TAREFAS = "tarefas.json"
 
@@ -88,10 +83,12 @@ def apagar_tarefa(id):
 
     return jsonify({"status": "apagada"})
 
+
 @app.after_request
 def add_headers(response):
     response.headers["ngrok-skip-browser-warning"] = "true"
     return response
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
